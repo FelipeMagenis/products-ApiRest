b400352dae87e3e34450e601cf3d7bf6e0aa20d7
Felipe Magenis <felipe.magenis@gmail.com>
2023-08-02 15:48:00 -0300

    Project started

---
+++ src/test/java/io/github/felipemagenis/productsapi/ProductsJavaApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.felipemagenis.productsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsJavaApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
